5d759aea053983344cb1bcc2d2650c9654189cfd
Rishad <50762398+rishadblack@users.noreply.github.com>
2024-08-29 18:14:01 +0600

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Livewire\Volt\Volt;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+
+Volt::route('test', 'test')->name('test');
+
+
+require __DIR__.'/auth.php';
